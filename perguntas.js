809ad4e648247e4195114b1c9c63b10c6faf8bd8
llaura09 <maria.mafia.ferreira@escola.pr.gov.br>
2025-08-14 15:34:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+'O primeiro presidente do Brasil foi  Marechal Deodoro da Fonseca.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o maior do orgão do copo humano?',
+'A pele'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual a função da clorofila?',
+    'Capturar a energia da luz solar para a fotossíntese.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior rio do mundo em volume de água?',
+    'O Rio Amazonas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o nome do deserto mais quente do mundo?',
+    'Deserto do Saara.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a fórmula para calcular a área de um círculo?',
+    'Área = $πr^2$.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Um número natural que só pode ser dividido por 1 e por ele mesmo.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual a função dos glóbulos brancos?',
+    'Defender o corpo de doenças e infecções.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é um eclipse lunar?',
+    'É um fenômeno que ocorre quando a Terra se alinha entre o Sol e a Lua, projetando sua sombra na Lua.'
+)
